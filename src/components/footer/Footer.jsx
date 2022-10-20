25ef469e98ec184688bf0dcea51c999c716fd4a6
EmiliaFontana <gracielafernandez222@outlook.es>
2022-10-20 12:34:33 -0300

    cambiecoloryiconos

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,11 +5,20 @@ import React from 'react'
 import '../../styles/index.css' //General
 import styles from './footer.module.css' //Module
 
+//iconos
+import face from '../../assets/icons/facebook.png';
+import insta from '../../assets/icons/instagram.png';
+import link from '../../assets/icons/linked.png';
+import twitter from '../../assets/icons/twitter.png';
 
 const Footer = () => {
   return (
     <>
       <footer className={styles.footer1}>©2022 Digital Booking</footer>
+      <img src={face} alt='facebook' />
+      <img src={insta} alt='instagram' />
+      <img src={link} alt='linked' />
+      <img src={twitter} alt='twitter' />
     </>
   )
 }
