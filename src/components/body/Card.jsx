07ace1907dd45f3fc9cf13cf7290e628dadc810c
agus <agusquerencio@gmail.com>
2022-10-23 14:57:38 -0300

    Se agrega hook useInput , jwt en Login

--- src/components/body/Card.jsx
+++ src/components/body/Card.jsx
@@ -1,8 +1,25 @@
 
 import styles from './body.module.css'
+import staricon from '../../assets/icons/star.png'
 
 
 const Card=({type, name, start, url, descripcion})=>{
+
+    /* react-dom.development.js:86 Warning: Functions are not valid as a React child. 
+    This may happen if you return a Component instead of
+     <Component /> from render. Or maybe you meant to call this function rather than return it. 
+     https://stackoverflow.com/questions/48458334/functions-are-not-valid-as-a-react-child-this-may-happen-if-you-return-a-compon */
+     const star = ({start})=>  {
+        const container= <div> </div>;
+        for (let index = 0; index <= start; index++) {
+           
+          container.innerHTML += <img src={staricon} alt='estrella '/> 
+        }
+       
+      
+
+        }
+        
 
     return( 
     <div className={styles.card}>
@@ -12,6 +29,10 @@ const Card=({type, name, start, url, descripcion})=>{
         <h3>{type}</h3>
         <p>{name}</p>
         <p>{start}</p>
+        <img src={staricon} alt='estrella '/> 
+        {star}
+      
+      
         <p>{descripcion}</p>
         <button type="submit">Ver mas</button>
         </div>
--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -7,6 +7,9 @@ import { useState } from "react";
 
 //Login en proceso con jwt
 
+// importamos el hook useInput para crear el objeto input 
+import useInput from '../helpers/useInput'
+
 export const Login = ()=>{
 
     // objeto usuario 
@@ -14,21 +17,56 @@ export const Login = ()=>{
     const [user,setUser]= useState({
         email:'',
         password:''
-    })
+    });
 
-    const handleSubmit = (e)=> {
+    const [error,setError]=useState(false)
+
+ // ya le pasamos el tipo por parametro , en el form asignamos las propiedades de este objeto 
+    const email= useInput('text');
+    const password= useInput('password');
+
+  /* hook que modifica a la propeidad email del objet usuario, esto no es necesario porque reutilizamos
+useInput 
+    const handleEmail= (e)=>{
+        setUser({...user, email:e.target.value})
+    }*/
+
+    // hook para guardar la sesión si esta todo ok 
+   /*  const handleSubmit = (e)=> {
         e.preventDefault();
-       
-        const handleSubmit = (e) => {
-            e.preventDefault()
+        if(user.email == '' || user.password == ''){
+          alert('complete correctamente los campos');
+        }else{
             setUser({
-                email: '',
+                email: e.target.value,
                 password: ''
             })
             localStorage.setItem('logged', JSON.stringify(user))
         }
+    } */
+
+     // hook para guardar la sesión si esta todo ok , ahora si guardamos los valores dentro del objeto usuario
+     // x ahora estan las validaciones de ejemplo aca 
+   
+     const handleSubmit = (e)=> {
+        e.preventDefault();
+        if(email.value == '' || password.value == ''){
+          setError(true);
+          window.location.reload();
+          setUser({
+            email: '',
+            password:'' 
+        })
+        }else{
+            setUser({
+                email: email.value,
+                password: password.value
+            })
+            localStorage.setItem('logged', JSON.stringify(user))
+        }
+    }
+
     
-    };
 
 
     return(
@@ -36,15 +74,13 @@ export const Login = ()=>{
         <Navbar/>
         <div className="f-col f-center">
 				
-
 				<div className="f-row f-center">
 					<form onSubmit={handleSubmit}>
-           
-          
 						<div className="f-row">
 							<div className="f-col">
 							    <label>Email</label>
-                                <input  />	
+
+                                <input value={email.value} onChange={email.onChange} />	
                             </div>
 						</div>
 
@@ -52,17 +88,13 @@ export const Login = ()=>{
 							<div className="f-col">
 								         
                             <label>Password</label>
-                            <input  />
-							</div>
-
-			
+                            <input value={password.value} onChange={password.onChange} />
+							</div>			
 						</div>
-
-					
-
 						<div className="f-row">
 							<div className="f-col">
 								<div style={{ marginTop: '30px' }}>
+                                    { error && <p>campos requeridos</p>}
 									<input type="submit" onClick={handleSubmit} className="btn btn-primary" value="ingresar" />
 								</div>
 								<Link to='/signin'>¿No tienes una cuenta? <span>Registrarme</span></Link>
@@ -76,5 +108,6 @@ export const Login = ()=>{
 
         </>
         )
+
 
 }
