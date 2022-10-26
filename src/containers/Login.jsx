9da9b035cc4b42e6d7b2eae9fff877c09bf60536
agus <agusquerencio@gmail.com>
2022-10-25 21:09:57 -0300

    [Add] se agregan las validaciones para el login

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -2,96 +2,116 @@ import Navbar from "../components/navbar/Navbar"
 import Footer from "../components/footer/Footer"
 
 //
-import { Link } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 import { useState } from "react";
 
-//Login en proceso con jwt
+
+//Login en proceso 
 
 // importamos el hook useInput para crear el objeto input 
-import useInput from '../hooks/useInput'
+import useInput from '../helpers/useInput'
+import { validateEmail , validatePass} from "../helpers/ValidateForms";
 
-export const Login = () => {
+export const Login = ()=>{
 
-	// objeto usuario 
+    //sesión guardada
+    const sesion={
+        email:"juan20@gmail.com",
+        password:"hola123@"
+    }
+    // objeto usuario 
 
-	const [user, setUser] = useState({
-		email: '',
-		password: ''
-	});
+    const [user,setUser]= useState({
+        email:'',
+        password:''
+    });
 
-	const [error, setError] = useState(false)
+    const [error,setError]=useState(false)
 
-	// ya le pasamos el tipo por parametro , en el form asignamos las propiedades de este objeto 
-	const email = useInput('text');
-	const password = useInput('password');
+ // ya le pasamos el tipo por parametro , en el form asignamos las propiedades de este objeto 
+    const mail= useInput('text');
+    const pass= useInput('password');
 
-	/* hook que modifica a la propeidad email del objet usuario, esto no es necesario porque reutilizamos
+  /* hook que modifica a la propeidad email del objet usuario, esto no es necesario porque reutilizamos
 useInput 
-		const handleEmail= (e)=>{
-				setUser({...user, email:e.target.value})
-		}*/
+    const handleEmail= (e)=>{
+        setUser({...user, email:e.target.value})
+    }*/
 
-	// hook para guardar la sesión si esta todo ok 
-	/*  const handleSubmit = (e)=> {
-			 e.preventDefault();
-			 if(user.email == '' || user.password == ''){
-				 alert('complete correctamente los campos');
-			 }else{
-					 setUser({
-							 email: e.target.value,
-							 password: ''
-					 })
-					 localStorage.setItem('logged', JSON.stringify(user))
-			 }
-	 } */
+    // hook para guardar la sesión si esta todo ok 
+   /*  const handleSubmit = (e)=> {
+        e.preventDefault();
+        if(user.email == '' || user.password == ''){
+          alert('complete correctamente los campos');
+        }else{
+            setUser({
+                email: e.target.value,
+                password: ''
+            })
+            localStorage.setItem('logged', JSON.stringify(user))
+        }
+    } */
 
-	// hook para guardar la sesión si esta todo ok , ahora si guardamos los valores dentro del objeto usuario
-	// x ahora estan las validaciones de ejemplo aca 
+     // hook para guardar la sesión si esta todo ok , ahora si guardamos los valores dentro del objeto usuario
+     // x ahora estan las validaciones de ejemplo aca 
+     // las validaciones deberían hacerse en una función aparte pero por falta de tiempo...
+     const handleSubmit = (e)=> {
+       
+        e.preventDefault();
+        localStorage.clear();
+        if(! validateEmail(mail) || ! validatePass(pass) || sesion.email !== mail.value ||
+         sesion.password !== pass.value)  {
+          setError(true);
+        window.location.reload();
+        // document.form.reset() ;
+          
+          setUser({
+            email: '',
+            password:'' 
+        })
+        }else{
+        
+            setUser({
+                email: mail.value,
+                password: pass.value
+            })
+            localStorage.setItem('user', JSON.stringify(user));
+        
+            //<Navigate to="./"/> 
+          //  window.location.replace('./')
+          // faltaría que guarde la sesión para poder modificar el header
+        }
+    }
 
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		if (email.value == '' || password.value == '') {
-			setError(true);
-			window.location.reload();
-			setUser({
-				email: '',
-				password: ''
-			})
-		} else {
-			setUser({
-				email: email.value,
-				password: password.value
-			})
-			localStorage.setItem('logged', JSON.stringify(user))
-		}
-	}
+    
 
-	return (
-		<>
-			<Navbar />
-			<div className="f-col f-center">
 
+    return(
+        <>
+        <Navbar/>
+        <div className="f-col f-center">
+				
 				<div className="f-row f-center">
 					<form onSubmit={handleSubmit}>
 						<div className="f-row">
 							<div className="f-col">
-								<label>Email</label>
+							    <label>Email</label>
 
-								<input value={email.value} onChange={email.onChange} />
-							</div>
+                                <input value={mail.value} onChange={mail.onChange} />	
+                            </div>
 						</div>
 
 						<div className="f-row">
 							<div className="f-col">
-
-								<label>Password</label>
-								<input value={password.value} onChange={password.onChange} />
-							</div>
+								         
+                            <label>Password</label>
+                            <input value={pass.value} onChange={pass.onChange} />
+							</div>			
 						</div>
 						<div className="f-row">
 							<div className="f-col">
 								<div style={{ marginTop: '30px' }}>
-									{error && <p>campos requeridos</p>}
+                                    { error?<p>campos requeridos</p>: <Navigate to="./"/> }
 									<input type="submit" onClick={handleSubmit} className="btn btn-primary" value="ingresar" />
 								</div>
 								<Link to='/signin'>¿No tienes una cuenta? <span>Registrarme</span></Link>
@@ -100,7 +120,11 @@ useInput
 					</form>
 				</div>
 			</div>
-			<Footer />
-		</>
-	)
+
+        <Footer/>
+
+        </>
+        )
+
+
 }
--- src/helpers/ValidateForms.js
+++ src/helpers/ValidateForms.js
@@ -7,3 +7,23 @@ export const ValidateFormHelper = (model) => {
     }
     return true;
 }
+
+
+
+/* agrego validaciones */
+
+export function validateEmail(email) {
+    const arroba= email.split("@");
+    //const regexEmail= /^[a-zA-Z0-9.!#$%&'*+=?^_`{|}~-]{0,25}@[a-zA-Z0-9-]{0,25}[.]([a-zA-Z]+){1,10}$ | ^[a-zA-Z0-9.!#$%&'*+=?^_`{|}~-]{0,25}@[a-zA-Z0-9-]{0,25}(?:\.[a-zA-Z]+){1,10}$/;
+    //!regexEmail.test(email.value)
+    if(email.value ==='' || email.length<10 || email.length >30 || parseInt(email)   || arroba.length !==1)
+    { return false}
+    else{ return true}
+}
+
+export function validatePass(pass) {
+    //const regexPass = /^(?=(?:.*\d){1})(?=(?:.*[A-Z]){1})(?=(?:.*[a-z]){1})(?=(?:.*[@$?¡\-_]){1})\S{6,16}$/
+    //!regexPass.test(pass.value)
+    if(pass.value ==='' || pass.length < 6 ){ return false}
+    else{ return true}
+}
